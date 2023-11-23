8647c1042d0ef7bd71d30fcc5bbe7059793002a3
angelusMZ19 <angelomontenegro200306@gmail.com>
2023-11-23 19:05:49 +0100

    avance de minas true

--- Pescaminas/js/pescaminas.js
+++ Pescaminas/js/pescaminas.js
@@ -1,26 +1,34 @@
 function iniciarPartida() {
-    let dimension = prompt("Ingrese la dimensión de la tabla");
-    crearTaulell(dimension);
+    let fil = prompt("Ingrese la fila de la tabla");
+    let col = prompt("Ingrese la tabla de la tabla");
+    crearTaulell(fil, col);
+    setMines(fil, col);
 }
 
-function crearTaulell(dimension) {
+function crearTaulell(fil, col) {
     let tabla = "";
     let numCasilla;
 
-    if (dimension < 10) {
-        dimension = 10;
+    if (fil < 10) {
+        fil = 10;
     }
-    if (dimension > 30) {
-        dimension = 30;
+    if (fil > 30) {
+        fil = 30;
+    }
+    if (col < 10) {
+        col = 10;
+    }
+    if (col > 30) {
+        col = 30;
     }
 
     tabla += '<table id="tabla">';
-    for (let i = 1; i <= dimension; i++) {
+    for (let i=0 ; i < fil; i++) {
         tabla += '<tr id="filas">';
         
-        for (let j = 1; j <= dimension; j++) {
-            numCasilla= i +j -1;
-            tabla += '<td id="celdas" data-mina="false" onclick="obreCasella(\''+ numCasilla + '\')"><img src="img/fons20px.jpg" id="imagen"/></td>';
+        for (let j=0; j < col; j++) {
+            // numCasilla= i +j -1;
+            tabla += `<td id="${i}-${j}" data-mina="false" data-num-minas=0 onclick="obreCasella(${i},${j})"><img src="img/fons20px.jpg" id="imagen"/></td>`;
         }
         tabla += '</tr>';
     }
@@ -28,9 +36,20 @@ function crearTaulell(dimension) {
     document.getElementById("taulell").innerHTML = tabla;
     return tabla;
 }
-function obreCasella(numCasilla) {
-    alert('se a abierto la casilla con el conetenido: ' + numCasilla);
+function obreCasella(i,j) {
+    let numCasilla= document.getElementById(`${i}-${j}`).innerHTML="";
+    console.log(`${i}-${j}`);
+    alert('se a abierto la casilla con el conetenido: ' + `${i}-${j}`);
+    
 }
-function setMines(){
+function setMines(fil, col){
+    let minas= Math.floor((fil*col)*0.17);
+    for(let a= 0; a< minas; a++){
+        let row = Math.floor(Math.random() * fil);
+        let colum = Math.floor(Math.random() * col);
+        let numCasilla= document.getElementById(`${row}-${colum}`);
+        numCasilla.dataset.mina="true";
+    }
+    console.log(minas);
 
 }
