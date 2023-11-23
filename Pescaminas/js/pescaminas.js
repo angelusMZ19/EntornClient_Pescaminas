6770e0646e8269fecebad2afa308415046d260aa
angelusMZ19 <angelomontenegro200306@gmail.com>
2023-11-23 16:57:40 +0100

    avanace

--- Pescaminas/js/pescaminas.js
+++ Pescaminas/js/pescaminas.js
@@ -1,10 +1,11 @@
 function iniciarPartida() {
     let dimension = prompt("Ingrese la dimensión de la tabla");
-    creaTabla(dimension);
+    crearTaulell(dimension);
 }
 
-function creaTabla(dimension) {
+function crearTaulell(dimension) {
     let tabla = "";
+    let numCasilla;
 
     if (dimension < 10) {
         dimension = 10;
@@ -16,12 +17,20 @@ function creaTabla(dimension) {
     tabla += '<table id="tabla">';
     for (let i = 1; i <= dimension; i++) {
         tabla += '<tr id="filas">';
+        
         for (let j = 1; j <= dimension; j++) {
-            tabla += '<td id="celdas" data-mina="false" ><img src="img/fons20px.jpg" id="imagen"/></td>';
+            numCasilla= i +j -1;
+            tabla += '<td id="celdas" data-mina="false" onclick="obreCasella(\''+ numCasilla + '\')"><img src="img/fons20px.jpg" id="imagen"/></td>';
         }
         tabla += '</tr>';
     }
     tabla += '</table>';
     document.getElementById("taulell").innerHTML = tabla;
     return tabla;
+}
+function obreCasella(numCasilla) {
+    alert('se a abierto la casilla con el conetenido: ' + numCasilla);
+}
+function setMines(){
+
 }
