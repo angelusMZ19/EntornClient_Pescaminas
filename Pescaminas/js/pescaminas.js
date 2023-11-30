6284a574916d3fe04ca59ed55a961d9b1b81370e
angelusMZ19 <angelomontenegro200306@gmail.com>
2023-11-30 23:57:24 +0100

    Pescaminas

--- Pescaminas/js/pescaminas.js
+++ Pescaminas/js/pescaminas.js
@@ -1,37 +1,38 @@
 let fil;
 let col;
+let minas;
+let opened = 0;
+let sinMinas;
 function iniciarPartida() {
     fil = prompt("Ingrese la fila de la tabla");
     col = prompt("Ingrese la tabla de la tabla");
     if (fil < 10) {
         fil = 10;
     }
-    if (fil > 30) {
-        fil = 30;
-    }
     if (col < 10) {
         col = 10;
+    }
+    if (fil > 30) {
+        fil = 30;
     }
     if (col > 30) {
         col = 30;
     }
+    sinMinas = fil * col - minas;
+    opened = 0;
+
     crearTaulell(fil, col);
     setMines(fil, col);
-    calcularAdyacentes(fil, col);
+    calcularAdyacentes(fil, col);  
 }
 
 function crearTaulell(fil, col) {
-    let tabla = "";
-    //let numCasilla;
-//hacer el tratamiento de las fil y col arriba en inicar partida
-    
-
+    let tabla = "";    
     tabla += '<table id="tabla">';
     for (let i=0 ; i < fil; i++) {
         tabla += '<tr id="filas">';
         
         for (let j=0; j < col; j++) {
-            // numCasilla= i +j -1;
             tabla += `<td id="${i}-${j}" data-mina="false" data-abierta="false" data-num-minas='0' onclick="obreCasella(${i},${j})"><img src="img/fons20px.jpg" id="${i},${j}"/></td>`;
         }
         tabla += '</tr>';
@@ -43,22 +44,30 @@ function crearTaulell(fil, col) {
 
 function obreCasella(i, j) {
     let numCasilla = document.getElementById(`${i}-${j}`);
+
+    if (numCasilla.dataset.abierta == "true") {
+        return;  // No hacer nada si la casilla ya está abierta
+    }
     numCasilla.innerHTML = " ";
-   
-    if (numCasilla.dataset.mina === "true") {
+    if (numCasilla.dataset.mina == "true") {
         numCasilla.innerHTML = `<img src="img/mina20px.jpg" id="${i},${j}"/>`;
-        setTimeout(function() {
-            alert("¡Has perdido! Se encontró una mina.")}, 500);
-       muestraMinas();
+        setTimeout(function () {
+            alert("¡BOM! Estás muerto");
+        }, 500);
+        muestraMinas();
     } else {
-        if(numCasilla.dataset.numMinas=='0'){
-            reveal();
-        }else{
-            numCasilla.innerHTML=numCasilla.dataset.numMinas;
+        if (numCasilla.dataset.numMinas == '0') {
+            reveal(i, j);
+        } else {
+            numCasilla.innerHTML = numCasilla.dataset.numMinas;
+        }
+        numCasilla.dataset.abierta = "true";
+        opened++;
+        if (opened == sinMinas) {
+            verificarGanador();
         }
     }
 }
-
 
 function reveal(i, j) {
     let casella = document.getElementById(`${i}-${j}`);
@@ -67,7 +76,7 @@ function reveal(i, j) {
     }
     casella.dataset.abierta="true"
     if(casella.dataset.numMinas=='0'){
-        casella.innerHTML=" ";
+        casella.innerHTML=`<img src="img/empty.png" id="${i},${j}"/>`;
         for (let k = i - 1; k <= i + 1; k++) {
             for (let l = j - 1; l <= j + 1; l++) {
                 if (k >= 0 && k < fil && l >= 0 && l < col) {
@@ -77,8 +86,7 @@ function reveal(i, j) {
         }
     }else{
         casella.innerHTML=casella.dataset.numMinas;
-    }
-    
+    }  
 }
 
 function esMina(i, j) {
@@ -98,18 +106,22 @@ function muestraMinas() {
     }
 }
 
-
-//recien agregado
-function setMines(fil, col){
-    let minas= Math.floor((fil*col)*0.17);
-    for(let a= 0; a< minas; a++){
+function setMines() {//evita que se reasigne una mina a una casilla que ya tiene
+    minas = Math.floor((fil * col) * 0.17);
+    console.log(minas);
+    for (let a = 0; a < minas; a++) {
         let row = Math.floor(Math.random() * fil);
         let colum = Math.floor(Math.random() * col);
-        let numCasilla= document.getElementById(`${row}-${colum}`);
-        numCasilla.dataset.mina="true";
+        while (document.getElementById(`${row}-${colum}`).dataset.mina === "true") {
+            row = Math.floor(Math.random() * fil);
+            colum = Math.floor(Math.random() * col);
+        }
+        let numCasilla = document.getElementById(`${row}-${colum}`);
+        numCasilla.dataset.mina = "true";
     }
 }
-//recien agregado
+
+
 function calcularAdyacentes(fil, col) {
     for (let i = 0; i < fil; i++) {
         for (let j = 0; j < col; j++) {
@@ -137,8 +149,7 @@ function setMinesAdjacents(i, j, minasAdyacentes) {
 }
 
 function setNumColor(casilla) {
-    let numMinas = parseInt(casilla.dataset.numMinas, 10);
-
+    let numMinas = parseInt(casilla.dataset.numMinas);
     switch (numMinas) {
         case 1:
             casilla.style.color = 'blue';
@@ -149,9 +160,26 @@ function setNumColor(casilla) {
         case 3:
             casilla.style.color = 'red';
             break;
-        // Puedes agregar más casos según sea necesario
         default:
             casilla.style.color = 'black';
             break;
+    }
+}
+
+function verificarGanador() {
+    let todasAbiertas = true;
+
+    for (let i = 0; i < fil; i++) {
+        for (let j = 0; j < col; j++) {
+            if (esMina(i, j) && document.getElementById(`${i}-${j}`).dataset.abierta !== "true") {
+                todasAbiertas = false;
+                break;
+            }
+        }
+    }
+    if (todasAbiertas) {
+        setTimeout(function () {
+            alert("¡Felicidades! Has ganado.");
+        }, 500);
     }
 }
